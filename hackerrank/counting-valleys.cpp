8a29dceb009bfed07529593dfa090d06a5048238
Dante Calderón <dantehemerson@gmail.com>
2018-11-26 21:20:06 -0500

    Create counting-valleys.cpp

---
+++ hackerrank/counting-valleys.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int countingValleys(int n, string s) {
+    bool down = false;
+    int m = 0;
+    int valleyCounter = 0;
+    for(int i = 0; i < n; i++) {
+        m = m + (s[i] == 'U' ? 1 : -1);
+        cout << m << endl;
+        if(m < 0) {
+            down = true;
+        }
+        if(m == 0 && down && s[i] != 'D') {
+            valleyCounter++;
+        }        
+    }
+    return valleyCounter;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string s;
+    getline(cin, s);
+
+    int result = countingValleys(n, s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
