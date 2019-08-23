b194ba31c8ca1c73bd3c0b6ef07ff99b40b8cadb
Dante Calderón <mail@dantecalderon.dev>
2019-08-23 11:26:34 -0500

    Update 486A. Calculating Function.cpp

--- Codeforces/486A. Calculating Function.cpp
+++ Codeforces/486A. Calculating Function.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 using namespace std;
+/**
+* If n is even, the result is n/2, as we se below.
+* If n is odd, make the same for (n - 1)(even) and substract n
+-1 +2 -3 +4 -5
+ \ /   \ /   |
+  +1   +1   -5
+    \ /      |
+ n/2 = 2    -5
+      \    /
+       [-3]
+**/
+
 
 int main() {
 	long long int n;
