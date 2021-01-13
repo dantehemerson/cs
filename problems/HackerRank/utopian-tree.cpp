9a38839bc48233f582f6e75b60afed299fee8e11
Dante Calderón <mail@dantecalderon.dev>
2021-01-13 02:44:40 -0500

    Create utopian-tree.cpp

---
+++ problems/HackerRank/utopian-tree.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the utopianTree function below.
+int utopianTree(int n) {
+    
+    // with series
+    // return n % 2 == 0 ? pow(2, n/2 + 1) - 1 : pow(2, (n + 1)/2 + 1) - 2;
+    
+    // with bitwise operations
+    return ~(~1 << (n >> 1)) << n % 2;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = utopianTree(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
