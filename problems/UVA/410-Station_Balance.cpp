3bbdf22d6f0f16cae70eda0c47b72099771049a5
Dante Calderon <mail@dantecalderon.dev>
2021-04-30 23:39:26 -0500

    Improvement

--- problems/UVA/410-Station_Balance.cpp
+++ problems/UVA/410-Station_Balance.cpp
@@ -23,36 +23,27 @@ int main() {
     vector<short> zeros(2 * C - S, 0);
     M.insert(M.begin(), zeros.begin(), zeros.end());
 
-    /** Save containers and calculate average */
-    vector<vector<short>> Cis(C, vector<short>());
+    /** calculate average */
     float average = 0;
     for(size_t i = 0; i < C; i++) {
       average += M[i] + M[M.size() - 1 - i];
-
-      if(M[i] > 0) Cis[i].push_back(M[i]);
-      Cis[i].push_back(M[M.size() - 1 - i]);
     }
     average /= C;
 
-    /** Calculate imbalance */
+    /** Calculate imbalance & Print result */
+    cout << "Set #" << times << endl;
+
     float Imbalance = 0.0;
-    for(size_t i = 0; i < C; i++) {
+    for(int i = 0; i < C; i++) {
+      cout << i << ": ";
+
+      if(M[i]) cout << " " << M[i];
+      cout << " " << M[M.size() - 1 - i] << endl;
+
       short Ci = M[i] + M[M.size() - 1 - i];
       Imbalance += float(abs(Ci - average));
     }
-
-    /** Print result */
-    cout << "Set #" << times << endl;
-    for(int i = 0; i < Cis.size(); i++) {
-      if(!Cis[i].empty()) {
-        cout << i << ":";
-        for(auto it = Cis[i].begin(); it != Cis[i].end(); it++) {
-          cout << " " << *it;
-        }
-        cout << endl;
-      }
-    }
-    cout << "IMBALANCE = " << std::fixed << std::setprecision(6) << Imbalance << endl;
+    cout << "IMBALANCE = " << std::fixed << std::setprecision(5) << Imbalance << endl;
   }
 
   return 0;
