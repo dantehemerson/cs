ef49bf69b877866980566ccae13ff211bf238251
Dante Calderon <mail@dantecalderon.dev>
2021-05-01 00:03:43 -0500

    Improved 3

--- problems/UVA/410-Station_Balance.cpp
+++ problems/UVA/410-Station_Balance.cpp
@@ -2,42 +2,25 @@
 
 using namespace std;
 
-void FAST_IO()
-{
-    ios_base::sync_with_stdio(0);
-    //cin.tie(NULL);
-    //cout.tie(NULL);
-    cout.setf(ios::fixed);
-    cout.precision(5);
-}
-
 int main() {
-
-  FAST_IO();
   int times = 0;
-  short C, S;
-  vector<short> M;
+  int C, S;
 
   while(cin >> C >> S, C) {
     times++;
-    M.clear();
+    vector<int> M(2 * C - S, 0);
 
-    short Mi;
+    int Mi;
     float average = 0;
     for(size_t i = 0; i < S; i++) {
       cin >> Mi;
       M.push_back(Mi);
       average += Mi;
     }
-    /** calculate average */
+
     average /= C;
 
     sort(M.begin(), M.end());
-
-    /** Insert zeros */
-    vector<short> zeros(2 * C - S, 0);
-    M.insert(M.begin(), zeros.begin(), zeros.end());
-
 
     /** Calculate imbalance & Print result */
     cout << "Set #" << times << endl;
@@ -49,8 +32,8 @@ int main() {
       if(M[i]) cout << " " << M[i];
       cout << " " << M[M.size() - 1 - i] << endl;
 
-      short Ci = M[i] + M[M.size() - 1 - i];
-      Imbalance += float(abs(Ci - average));
+      int Ci = M[i] + M[M.size() - 1 - i];
+      Imbalance += abs(Ci - average);
     }
     cout << "IMBALANCE = " << std::fixed << std::setprecision(5) << Imbalance << endl;
   }
