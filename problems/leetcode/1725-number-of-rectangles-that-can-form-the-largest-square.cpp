1988af129db4742354806ee09e2a768126554402
Dante Calderón <mail@dantecalderon.dev>
2021-05-02 18:20:06 -0500

    Improve - without Map

--- problems/leetcode/1725-number-of-rectangles-that-can-form-the-largest-square.cpp
+++ problems/leetcode/1725-number-of-rectangles-that-can-form-the-largest-square.cpp
@@ -1,14 +1,20 @@
-// Greedy
+// Greedy - Runtime 32ms FT 98.20
 class Solution {
 public:
     int countGoodRectangles(vector<vector<int>>& rectangles) {
-        map<int, int> L;
-        int maxLen;
+        int largest = 0, count = 0, maxLen;
+
         for(auto it = rectangles.begin(); it != rectangles.end(); it++) {
           maxLen = min(it->at(0), it->at(1));
-          L[maxLen]++;
+          
+          if(maxLen > largest) {
+            largest = maxLen;
+            count = 0;
+          }
+          
+          if(maxLen == largest) count++;
         }
       
-        return L.rbegin()->second;
+        return count;
     }
 };
