0f6b7b05259f62fb443dd3865a14bf8fb38baa1a
Dante Calderón <mail@dantecalderon.dev>
2021-11-05 00:06:41 -0500

    Create 53. Maximum Subarray.cpp

---
+++ problems/leetcode/53. Maximum Subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) { 
+        int sum = nums[0]; // sum for every subarray possible
+        int maxSum = sum;
+        for(int i = 1; i < nums.size(); i++) {
+          sum += nums[i];
+          if(nums[i] > sum) sum = nums[i];
+          if(sum > maxSum) maxSum = sum;
+        }
+      
+        return maxSum;
+    }
+};
