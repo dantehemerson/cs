3e2c2b216a7830d8f51fdae7d3a2765a4ebc04d1
Dante Calderón <mail@dantecalderon.dev>
2021-11-04 20:31:18 -0500

    Update 1. Two Sum.cpp

--- problems/leetcode/1. Two Sum.cpp
+++ problems/leetcode/1. Two Sum.cpp
@@ -1,5 +1,6 @@
 /**
 * O(n) solution
+16 ms	12.3 MB
 */
 
 class Solution {
@@ -17,5 +18,29 @@ public:
         }
       }
       return {};
+    }
+};
+
+
+/**
+O(n)
+Runtime: 8 ms, faster than 95.31% of C++ online submissions for Two Sum.
+Memory Usage: 11.3 MB, less than 18.29% of C++ online submissions for Two Sum.
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      map<int, int> rests;
+      for (int i = 0; i < nums.size(); i++) {
+        int missing = target - nums[i];
+
+        if (rests.count(missing)) {
+          return {rests[missing], i};
+        } else {
+          rests[nums[i]] = i;
+        }
+      }
+      return {}; // Never gets here
     }
 };
