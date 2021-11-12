faa53ae1b8d7c98261cb9b7b9ed8bcc8fe7bf635
Dante Calderón <mail@dantecalderon.dev>
2021-11-12 18:06:35 -0500

    Update 242. Valid Anagram.cpp

--- problems/leetcode/242. Valid Anagram.cpp
+++ problems/leetcode/242. Valid Anagram.cpp
@@ -7,3 +7,24 @@ public:
         return s == t;
     }
 };
+
+
+// Faster solution
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> counterMagazine(26);
+      
+        if(s.size() != t.size()) return false;
+      
+        for(char &c: s) {
+            counterMagazine[c - 'a']++;
+        }
+        
+        for(char &c: t) {
+            if(!counterMagazine[c - 'a']--) return false;
+        }
+        
+        return true;
+    }
+};
