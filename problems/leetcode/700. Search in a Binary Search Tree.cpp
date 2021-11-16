ea6e9d7c80208f5d98f8e1403b47f6fc13ce033e
Dante Calderón <mail@dantecalderon.dev>
2021-11-16 02:17:15 -0500

    Update 700. Search in a Binary Search Tree.cpp

--- problems/leetcode/700. Search in a Binary Search Tree.cpp
+++ problems/leetcode/700. Search in a Binary Search Tree.cpp
@@ -12,9 +12,7 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode*& root, int& val) {
-        if(!root) return nullptr;
-        if(root->val == val) return root;
-        
+        if(!root || root->val == val) return nullptr;
         
         auto leftSearch = searchBST(root->left, val);
         if(leftSearch) return leftSearch;
