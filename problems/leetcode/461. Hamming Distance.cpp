e4ddcfc597e5a3f033254656353822593b8ffe00
Dante Calderón <mail@dantecalderon.dev>
2021-11-19 02:26:23 -0500

    Create 461. Hamming Distance.cpp

---
+++ problems/leetcode/461. Hamming Distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int counter = 0;
+        while(x || y) {
+          if(x % 2 + y % 2 == 1) counter++;
+          
+          x >>= 1;
+          y >>= 1;
+        }
+        return  counter;
+    }
+};
