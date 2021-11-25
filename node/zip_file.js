137c123cf19cec6e20a6c107f00812b946b426c3
Dante Calderon <mail@dantecalderon.dev>
2021-11-24 23:18:08 -0500

    Filing

---
+++ node/zip_file.js
@@ -0,0 +1,20 @@
+const { pipeline } = require('stream');
+const fs = require('fs');
+const zlib = require('zlib');
+
+// Use the pipeline API to easily pipe a series of streams
+// together and get notified when the pipeline is fully done.
+// A pipeline to gzip a potentially huge video file efficiently:
+
+pipeline(
+  fs.createReadStream('/home/dantehemerson/Downloads/black_widow.mkv'),
+  zlib.createGzip(),
+  fs.createWriteStream('black_widow2.mkv.gz'),
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed', err);
+    } else {
+      console.log('Pipeline succeeded');
+    }
+  }
+);
