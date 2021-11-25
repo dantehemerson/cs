18147200cc9c9ec0050a3a6ce4d769684a5f692d
Dante Calderon <mail@dantecalderon.dev>
2021-11-24 23:35:19 -0500

    with captureRejections

---
+++ node/event-emitters-error-handling.js
@@ -0,0 +1,23 @@
+const { EventEmitter } = require('events')
+
+const ee1 = new EventEmitter({ captureRejections: true });
+
+ee1.on('something', async (value) => {
+  throw new Error('kaboom');
+});
+
+ee1.on('error', error => {
+
+  console.log('Carritooooos', error)
+});
+
+
+// ee1.emit('something')
+
+const ee2 = new EventEmitter({ captureRejections: true });
+ee2.on('something', async (value) => {
+  throw new Error('kaboom');
+});
+
+ee2[Symbol.for('nodejs.rejection')] = console.log;
+ee2.emit('something')
