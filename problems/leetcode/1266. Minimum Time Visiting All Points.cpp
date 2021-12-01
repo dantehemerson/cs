060d02f50875f792e540ca07d8fa7b25a310ed87
Dante Calderón <mail@dantecalderon.dev>
2021-12-01 01:46:24 -0500

    Update 1266. Minimum Time Visiting All Points.cpp

--- problems/leetcode/1266. Minimum Time Visiting All Points.cpp
+++ problems/leetcode/1266. Minimum Time Visiting All Points.cpp
@@ -3,10 +3,8 @@ public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int seconds = 0;
         for(int i = 0; i < points.size() - 1; i++) {
-          auto point = points[i];
-          
-          int diago = min(abs(point[0] - points[i  + 1][0]), abs(point[1] - points[i  + 1][1]));
-          int direct = max(abs(point[0] - points[i  + 1][0]), abs(point[1] - points[i  + 1][1])) - diago;
+          int diago = min(abs(points[i][0] - points[i  + 1][0]), abs(points[i][1] - points[i  + 1][1]));
+          int direct = max(abs(points[i][0] - points[i  + 1][0]), abs(points[i][1] - points[i  + 1][1])) - diago;
           
           seconds += diago + direct;
         }
