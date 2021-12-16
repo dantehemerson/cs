3fbb20fce4d89f18c6fe30b36e2c46bfe268c8b1
Dante Calderón <mail@dantecalderon.dev>
2021-12-15 23:35:20 -0500

    Update 19. Remove Nth Node From End of List.cpp

--- problems/leetcode/19. Remove Nth Node From End of List.cpp
+++ problems/leetcode/19. Remove Nth Node From End of List.cpp
@@ -8,35 +8,25 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  
- Two pointer implementation
- 
-fast pointer walk n steps
-slow pointer walk 1 steps until we get the correct position.
+  Two pointers O(N)
  */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* tempHead = new ListNode(0, head);
-        ListNode *slow = tempHead, *fast = tempHead;
+        ListNode *slow = tempHead, *fast = head;
+      
+        // Move fast in front so that the gap between slow and fast becomes n
+        for(int i = 0; i < n; i++) fast = fast->next;
        
-        int fastCounter = 0, slowCounter = 0;
-        int total = 1000;
-        while(slow->next) {
-            // fast walk n steps
-            for(int i = 0; i < n && fast->next; i++) {
-              fastCounter++;
-              fast = fast->next;
-            }
-            if(!fast->next) total = fastCounter;
-          
-            if(slowCounter == total - n) {
-               slow->next = slow->next->next;
-               break;
-            }
-            
-            slow = slow->next;
-            slowCounter++;
+        // Move fast to the end, maintaining the gap
+        while(fast) {
+          fast = fast->next;
+          slow = slow->next;
         }
+      
+        // Skip the desired node
+        slow->next = slow->next->next; 
       
         return tempHead->next;
     }
