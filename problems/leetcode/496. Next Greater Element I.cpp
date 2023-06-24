9f980266f43c7d243298e5288262405d5bd1f5ec
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-06-24 00:47:55 -0500

    fixed shit that havent been fixed in last commit

---
+++ problems/leetcode/496. Next Greater Element I.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        stack<int> s;
+        unordered_map<int, int> gt;
+
+        for (int i = 0; i < nums2.size(); i++) {
+            while (!s.empty() && s.top() < nums2[i]) {
+                gt[s.top()] = nums2[i];
+                s.pop();
+            }
+
+            s.push(nums2[i]);
+        }
+
+
+        vector<int> result(nums1.size(), -1);
+        for (int i = 0; i < nums1.size(); i++)  {
+            if(gt.count(nums1[i])) {
+                result[i] = gt[nums1[i]];
+            }
+        }
+
+        return result;
+    }
+}
