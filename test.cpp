3fd6e925cd7e9141c88ec5e75f3db7bb270c1b2f
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-06-24 14:06:34 -0500

    update .gitignore

--- test.cpp
+++ test.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
+void changeValue(int a) {
+  a = 10;
+}
+
 int main() {
-  cout << "hello" << endl;
+  int a = 42;
+  cout << "a = " << a << endl;
 
+  changeValue(a);
 
+  cout << "a = " << a << endl;
 	return 0;
 }
