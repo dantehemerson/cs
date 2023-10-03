9df17301eb469c78fa769df25fd638ddf68da6bb
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-10-02 22:55:33 -0500

    don’t even try to refactor it.

--- problems/leetcode/567. Permutation in String.cpp
+++ problems/leetcode/567. Permutation in String.cpp
@@ -29,14 +29,10 @@ public:
 };
 
 
-/** (BEST SOLUTION)
-  Using arr of english lowecase letters to check permutation
-
-  O(n * k) little bit better than previous
-
-  n: length of s2
-  k: length of s1
+/**
+  BEST SOLUTION:
 */
+
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
@@ -46,19 +42,19 @@ public:
 
        int countS1[26] = { 0 };
 
-
        for (int i = 0; i < s1.size(); i++) {
            countS1[s1[i] - 'a']++;
         }
 
-        int countSub[26];
+        int countSub[26] = { 0 };
 
-        for (int i = 0; i <= s2.size() - k; i++) {
-            for (int j = 0; j < 26; j++) countSub[j] = 0;
+        int l = 0;
+        int r = 0;
 
-            for (int j = i; j < i + k; j++) {
-                countSub[s2[j] - 'a']++;
-            }
+        for (r = 0; r < s1.size() - 1; r++) countSub[s2[r] - 'a']++;
+
+        while (r < s2.size()) {
+            countSub[s2[r] - 'a']++;
 
             bool isPermutation = true;
             for (int v = 0; v < 26; v++) {
@@ -71,55 +67,11 @@ public:
             if (isPermutation) {
                 return true;
             }
-        }
 
-        return false;
-    }
-}
+            countSub[s2[l] - 'a']--;
 
-
-/**
-  Using hash table to check permutation
-
-  O(n * k) little bit better than previous
-
-  n: length of s2
-  k: length of s1
-*/
-class Solution {
-public:
-    bool checkInclusion(string s1, string s2) {
-        if (s1.size() > s2.size()) return false;
-
-        int k = s1.size();
-
-        unordered_map<char, int> countS1;
-        for (int i = 0; i < s1.size(); i++) {
-           countS1[s1[i]]++;
-        }
-
-        unordered_map<char, int> countSub;
-
-        for (int i = 0; i <= s2.size() - k; i++) {
-            countSub.clear();
-
-            for (int j = i; j < i + k; j++) {
-                countSub[s2[j]]++;
-            }
-
-            if (countSub.size() != countS1.size()) continue;
-
-            bool isPermutation = true;
-            for (const std::pair<const char, int>& pair : countS1) {
-                if (pair.second != countSub[pair.first]) {
-                    isPermutation = false;
-                    break;
-                }
-            }
-
-            if (isPermutation) {
-                return true;
-            }
+            l++;
+            r++;
         }
 
         return false;
