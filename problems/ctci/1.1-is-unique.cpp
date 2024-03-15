dda61849076e9c386ae714e1e7ef31ab86a47fda
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-03-15 16:38:21 -0500

    Add solution

--- problems/ctci/1.1-is-unique.cpp
+++ problems/ctci/1.1-is-unique.cpp
@@ -1,24 +1,34 @@
+// Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+
+// O(n log n) Using sorting: https://gist.github.com/dantehemerson/a8b595b68bdaad628786a714e4da187b
+
+// Implementation O(n) and space O(1). Assuming only ASCII is supported.
 #include <bits/stdc++.h>
 #include <iostream>
 
 using namespace std;
 
 bool isUnique(string s) {
-  unordered_map<char, int>  counter;
+  bitset<256> counter;
 
-  for (char &c : s) {
-    if (counter.count(c) > 0) {
+  for (char &c: s) {
+    if (counter.test(int(c))) {
       return false;
     }
-    counter[c]++;
+    counter.set(int(c));
+
+    // cout << "char: " << c << " : " << code << " -- " << result << " -- " << bitset<256>(result) << endl;
   }
 
   return true;
 }
 
 int main() {
-  cout << "abc: " <<  isUnique("abc") << endl;
+  cout << "012: " <<  isUnique("012") << endl;
+  cout << "abz: " <<  isUnique("abz") << endl;
   cout << "aba: " << isUnique("aba") << endl;
 
   return 0;
 }
+
