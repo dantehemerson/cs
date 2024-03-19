f1d07b31a00904269fd131b4d95dd14d780986f8
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-03-18 22:20:37 -0500

    Update

---
+++ problems/thcormen/12.1-3.non-recursive-in-order-bin-tree.cpp
@@ -0,0 +1,82 @@
+/**
+12.1-3 Give a nonrecursive algorithm that performs an inorder tree walk. (Hint: An easy solution uses a stack as an auxiliary data structure. A more complicated, but ele- gant, solution uses no stack but assumes that we can test two pointers for equality.)
+*/
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Node {
+public:
+  Node(int _val):val(_val) {}
+
+  Node* left = nullptr;
+  Node* right = nullptr;
+  int val;
+};
+
+void inOrderTreeWalk(Node* root) {
+  if (root) {
+    inOrderTreeWalk(root->left);
+    cout << root->val << " ";
+    inOrderTreeWalk(root->right);
+  }
+}
+
+void inOrderTreeWalkNonRecursiveStack(Node* root) {
+  Node* current = root;
+  stack<Node*> stack;
+
+
+  while (current) {
+    stack.push(current);
+
+    current = current->left;
+
+    while (current == nullptr && !stack.empty()) {
+      Node* top = stack.top();
+      stack.pop();
+
+      cout << top->val << " ";
+
+      current = top->right;
+    }
+  }
+}
+
+
+int main() {
+
+  /** Tree:
+         6
+       /   \
+      5     7
+     / \     \
+    2   5     8
+  */
+  auto root = new Node(6);
+  root->left = new Node(5);
+  root->left->left = new Node(2);
+  root->left->right = new Node(5);
+  root->right = new Node(7);
+  root->right->right = new Node(8);
+
+
+  cout << "In Order Tree Walk (recursive): " << endl;
+  inOrderTreeWalk(root);
+  cout << endl;
+
+
+  cout << "In Order Tree Walk (non Recursive with stack): " << endl;
+  inOrderTreeWalkNonRecursiveStack(root);
+  cout << endl;
+
+
+
+
+
+
+  return 0;
+}
+
