f70b048dc83f226125f80b9e5b13aee39eedec93
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-03-20 23:15:20 -0500

    Update

--- problems/thcormen/12.1-3.non-recursive-in-order-bin-tree.cpp
+++ problems/thcormen/12.1-3.non-recursive-in-order-bin-tree.cpp
@@ -1,5 +1,10 @@
 /**
-12.1-3 Give a nonrecursive algorithm that performs an inorder tree walk. (Hint: An easy solution uses a stack as an auxiliary data structure. A more complicated, but ele- gant, solution uses no stack but assumes that we can test two pointers for equality.)
+Exercise 12.1-3
+
+Give a nonrecursive algorithm that performs an inorder tree walk.
+
+(Hint: An easy solution uses a stack as an auxiliary data structure.
+  A more complicated, but ele- gant, solution uses no stack but assumes that we can test two pointers for equality.)
 */
 #include <bits/stdc++.h>
 #include <iostream>
--- problems/thcormen/12.1-4.recursive-preorder&postorder.cpp
+++ problems/thcormen/12.1-4.recursive-preorder&postorder.cpp
@@ -1,5 +1,7 @@
 /**
-12.1-3 Give a nonrecursive algorithm that performs an inorder tree walk. (Hint: An easy solution uses a stack as an auxiliary data structure. A more complicated, but ele- gant, solution uses no stack but assumes that we can test two pointers for equality.)
+Exercise 12.1-4
+
+Give recursive algorithms that perform preorder and postorder tree walks in ‚.n/ time on a tree of n nodes.
 */
 #include <bits/stdc++.h>
 #include <iostream>
