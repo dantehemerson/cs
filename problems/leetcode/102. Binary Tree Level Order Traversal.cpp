daeb48b8c7eb1fcdba58b4ab89b128bb33d9ae10
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-03-24 14:35:19 -0500

    Update

--- problems/leetcode/102. Binary Tree Level Order Traversal.cpp
+++ problems/leetcode/102. Binary Tree Level Order Traversal.cpp
@@ -9,6 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //  BFS: Time: O(n) Space: O(n)
+class Solution {
+public:
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> vec;
+
+        if(!root) return vec;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> levelVector(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* u = q.front(); q.pop();
+
+                levelVector[i] = u->val;
+
+                if (u->left) {
+                    q.push(u->left);
+                }
+
+                if (u->right) {
+                    q.push(u->right);
+                }
+            }
+
+            vec.push_back(levelVector);
+        }
+
+        return vec;
+    }
+};
+
 //  Time: O(n) Space: O(n)
 class Solution {
 public:
